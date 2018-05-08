d20f0996bfd185694b95676626864af2e5846ea8
sam <amazing_sam@outlook.com>
2018-05-08 11:01:49 +0800

    bad day

---
+++ Tutor.UI/ViewModels/INdexVM.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Model;
+
+namespace Tutor.UI.ViewModels
+{
+    public class IndexVM
+    {
+        public IEnumerable<grade> grade
+        {
+            get; set;
+        }
+        public IEnumerable<subject> subject
+        {
+            get; set;
+        }
+        public IEnumerable<Teacher> teacher
+        {
+            get; set;
+        }
+        public IEnumerable<TeacherInfo> teacherinfo
+        {
+            get; set;
+        }
+        public IEnumerable<Task> task
+        {
+            get;set;
+        }
+    }
+}
