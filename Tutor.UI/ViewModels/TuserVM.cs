75e0bd98bad1e6d85481c90b57ce0dd9ad649def
sam <amazing_sam@outlook.com>
2018-05-24 23:35:20 +0800

    好累 524

---
+++ Tutor.UI/ViewModels/TuserVM.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Model;
+
+namespace Tutor.UI.ViewModels
+{
+    public class TuserVM
+    {
+        public TeacherInfo teacherinfo
+        {
+            get;set;
+        }
+        public Exhibition exhibition
+        {
+            get; set;
+        }
+        public Teacher teacher
+        {
+            get;set;
+        }
+    }
+}
