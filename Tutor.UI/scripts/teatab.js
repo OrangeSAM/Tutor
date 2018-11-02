61cef06dd3a117a8f76dee8884852cd93ecebc36
sam <amazing_sam@outlook.com>
2018-11-02 19:25:56 +0800

    明天答辩

---
+++ Tutor.UI/scripts/teatab.js
@@ -0,0 +1,14 @@
+﻿var tabs = $(".leftnavbar li");
+var rightbox = $(".rightdetail>div>div");
+for (i = 0; i < tabs.length; i++) {
+    var tab = tabs[i];
+    setTabHander(tab, i);
+}
+function setTabHander(tab, tabpos) {
+    tab.onclick = function () {
+        for (i = 0; i < rightbox.length; i++) {
+            rightbox[i].className = 'unactive';
+        }
+        rightbox[tabpos].className = 'activerg';
+    }
+}
