4fe97dff44b9d94c9e959aec5b2dfaa5a75bf5b6
sam <amazing_sam@outlook.com>
2019-02-27 15:48:24 +0800

    finished the courses display

---
+++ Tutor.UI/Controllers/CoursesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Model;
+using BLL;
+using Tutor.UI.ViewModels;
+
+namespace Tutor.UI.Controllers
+{
+    public class CoursesController : Controller
+    {
+        CoursesService courSer = new CoursesService();
+        // GET: Courses
+        public ActionResult Index()
+        {
+            var course = courSer.GetModels(b => b.Course_id != 0);
+            CoursesVM coursesvm = new CoursesVM();
+            coursesvm.courses = course;
+            return View(coursesvm);
+        }
+    }
+}
---
+++ Tutor.UI/ViewModels/CoursesVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Model;
+
+namespace Tutor.UI.ViewModels
+{
+    public class CoursesVM
+    {
+
+        public IEnumerable<courses> courses
+        {
+            get;set;
+        }
+    }
+}
